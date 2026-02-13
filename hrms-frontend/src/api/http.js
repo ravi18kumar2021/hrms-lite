a0bf817021c6c566a8a82bfd0859efed21e4624b
ravi <programmer.ravi.2022@gmail.com>
2026-02-13 23:30:28 +0530

    .env deleted

--- hrms-frontend/src/api/http.js
+++ hrms-frontend/src/api/http.js
@@ -1,5 +1,5 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
+console.log('API BASE URL: ', BASE_URL);
 export async function apiFetch(endpoint, options={}) {
     const response = await fetch(`${BASE_URL}/${endpoint}`, {
         headers: {
