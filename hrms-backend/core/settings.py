61a0b2705417adb54cfe4be4c1c5651b72255b2c
ravi <programmer.ravi.2022@gmail.com>
2026-02-13 23:03:11 +0530

    cors added

--- hrms-backend/core/settings.py
+++ hrms-backend/core/settings.py
@@ -132,4 +132,7 @@ MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://hrms-lite-red.vercel.app/'
+]
